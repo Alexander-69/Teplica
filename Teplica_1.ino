ea707013064fbf84bfc3bd10dbc8af357509b72a
Alexander-69 <139266453+Alexander-69@users.noreply.github.com>
2024-02-11 17:47:56 +0300

    Внесены кнопки и РЕЛЕ

--- Teplica_1.ino
+++ Teplica_1.ino
@@ -1,130 +1,151 @@
-//поправил код
+
 //Подключение датчиков температуры
 #include "DHT.h"
 
-#define DHT1PIN 2 //Подключение первого датчика температуры на пин
-#define DHT2PIN 4 //Подключение второго датчика температуры на пин
-#define DHT3PIN 16 //Подключение третьего датчика температуры на пин
-#define DHTTYPE DHT22   // Выбор типа датчика DHT 11 DHT 22  (AM2302), AM2321
-DHT dht(DHT1PIN, DHTTYPE);  //определяем первый датчик температуры
-DHT dht2(DHT2PIN, DHTTYPE); //определяем второй датчик температуры
-DHT dht3(DHT3PIN, DHTTYPE); //определяем третий датчик температуры
-
-#define RELE_1 13 //Подключение первого реле
-#define RELE_2 12 //Подключение второго реле
-#define KONCEVIK_1 14 //Подключение концевика открыто
-#define KONCEVIK_2 27 //Подключение концевика закрыто
-#define BUTTON_3 26 //Подключение кнопки на открытие
-#define BUTTON_4 25 //Подключение кнопки на закрытие
+#define DHT1PIN 2            //Подключение первого датчика температуры на пин
+#define DHT2PIN 4            //Подключение второго датчика температуры на пин
+#define DHT3PIN 16           //Подключение третьего датчика температуры на пин
+#define DHTTYPE DHT22        // Выбор типа датчика DHT 11 DHT 22  (AM2302), AM2321
+DHT dht1(DHT1PIN, DHTTYPE);   //определяем первый датчик температуры
+DHT dht2(DHT2PIN, DHTTYPE);  //определяем второй датчик температуры
+DHT dht3(DHT3PIN, DHTTYPE);  //определяем третий датчик температуры
+//---------Определение кнопок и концевиков для окон----------------------------------
+#define RELE_1 14      //Подключение первого реле
+#define RELE_2 27      //Подключение второго реле
+#define KONCEVIK_1 13  //Подключение концевика открыто
+#define KONCEVIK_2 12  //Подключение концевика закрыто
+#define BUTTON_3 26    //Подключение кнопки на открытие
+#define BUTTON_4 25    //Подключение кнопки на закрытие
 
 #define ON 0
 #define OFF 1
 
+#define DEBUG_DHT
+
 //Для кнопок
 #include "GyverButton.h"
-GButton butt1(KONCEVIK_1);
-GButton butt2(KONCEVIK_2);
+GButton KONC_1(KONCEVIK_1);
+GButton KONC_2(KONCEVIK_2);
 GButton butt3(BUTTON_3);
 GButton butt4(BUTTON_4);
+
+unsigned long ms = 0, dht22Ms = 0, windowMs = 0;
+int window = 0; //автомат работы двигателя открытия окон
 
 
 void setup() {
   Serial.begin(115200);
   Serial.println(F("DHT22 test!"));
 
-//Отключаем реле на старте
 
+  KONC_1.setTickMode(AUTO);
+  KONC_2.setTickMode(AUTO);
+  butt3.setTickMode(AUTO);
+  butt4.setTickMode(AUTO);
 
-    butt1.setTickMode(AUTO);
-      butt2.setTickMode(AUTO);
-      butt3.setTickMode(AUTO);
-      butt4.setTickMode(AUTO);
+  //Включаем управление модулем реле
+  pinMode(RELE_1, OUTPUT);  //Управление реле 1
+  pinMode(RELE_2, OUTPUT);  //Управление реле 2
 
-//Включаем управление модулем реле
-  pinMode (RELE_1, OUTPUT); //Управление реле 1
-  pinMode (RELE_2, OUTPUT); //Управление реле 2
+  //Отключаем реле на старте
+  digitalWrite(RELE_1, OFF);
+  digitalWrite(RELE_2, OFF);
 
-//Отключаем реле на старте
-digitalWrite (RELE_1, OFF);
-digitalWrite (RELE_2, OFF);
-
-//Датчики температуры
-  dht.begin();
+  //Датчики температуры
+  dht1.begin();
   dht2.begin();
   dht3.begin();
 
-    
 
-} //setup
+
+}  //setup
 
 void loop() {
-   delay(2000); // Пауза в 2 секунды
-  // Reading temperature or humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-//---------------------------------------------------------------------------------------------------
-//Подключение кнопок и концевиков для реле 1
-  if (butt1.isClick()) Serial.println("Button 1"); {
+  ms = millis();
+
+  // раз в 2 секунды опрашиваем датчики
+  if ((ms - dht22Ms) > 2000) {
+    dht22Ms = ms;
+  }
+    int h1 = dht1.readHumidity();
+    int h2 = dht2.readHumidity();
+    int h3 = dht3.readHumidity();
+    // Чтение температуры в цельсиях
+    int t1 = dht1.readTemperature();
+    int t2 = dht2.readTemperature();
+    int t3 = dht3.readTemperature();
+  
+
+ 
+ switch(window){
+  case 0:
+  // инициализация, ничего не делаем
+  windowMs =0;
+  window = 5;
+  break;
+  case 5:
+  
+  break;
+  case 10:
+  break;
+ }
+ 
+  
+  //---------------------------------------------------------------------------------------------------
+  //Подключение кнопок и концевиков для реле 1
+   if (KONC_1.isClick()) {
+    Serial.println("KONCEVIK_1");
     digitalWrite (RELE_1, OFF);
   }
-  if (butt2.isClick()) Serial.println("Button 2"); {
+  if (KONC_2.isClick()) {
+    Serial.println("KONCEVIK_2");
     digitalWrite (RELE_2, OFF);
   }
-  if (butt3.isClick()) Serial.println("Button 3"); {
+  if (butt3.isClick()) {
+    Serial.println("BUTTON_3");
     digitalWrite (RELE_1, ON);
   }
-  if (butt4.isClick()) Serial.println("Button 4"); {
+  if (butt4.isClick()) {
+    Serial.println("BUTTON_4");
     digitalWrite (RELE_2, ON);
   }
-//---------------------------------------------------------------------------------------------------
+  //---------------------------------------------------------------------------------------------------
 
-  float h = dht.readHumidity();
-  float h2 = dht2.readHumidity();
-  float h3 = dht2.readHumidity();
-  // Чтение температуры в цельсиях
-  float t = dht.readTemperature();
-  float t2 = dht2.readTemperature();
-  float t3 = dht3.readTemperature();
-   
-  // Проверка датчиков температуры
-  if (isnan(h) || isnan(t)) {  //Проверка первого датчика температуры
-    Serial.println(F("Failed to read from DHT_1 sensor!"));
-    return;
-  } //if первого датчика температуры
-  if (isnan(h2) || isnan(t2)) {   //Проверка второго датчика температуры
-    Serial.println(F("Failed to read from DHT_2 sensor!"));
-    return;
-  } //if второго датчика температуры
-  if (isnan(h3) || isnan(t3)) {   //Проверка третьего датчика температуры
-    Serial.println(F("Failed to read from DHT_2 sensor!"));
-    return;
-  } //if третьего датчика температуры
-    
-//    float hic = dht.computeHeatIndex(t, h, false);
-  //Выдача информации о температуре и влажности в серийный порт
-  Serial.print(F("Humidity1: "));
-  Serial.print(h);
-  Serial.print(F("% Temperature: "));
-  Serial.print(t);
-  Serial.println(F("°C "));
-//  Serial.print(hic);
-//  Serial.print(F("°C "));
+
+
+//-------------Датчики температуры и влажности воздуха-------------------------------------------------------------------
+  //Проверка датчиков температуры
+  if (isnan(h1) || isnan(t1)) {  //Проверка первого датчика температуры
+    Serial.println(F("DHT_1 Error!"));
+      }//if первого датчика температуры
+  if (isnan(h2) || isnan(t2)) {  //Проверка второго датчика температуры
+    Serial.println(F("DHT_2 Error!"));
+  }//if второго датчика температуры
+  if (isnan(h3) || isnan(t3)) {  //Проверка третьего датчика температуры
+    Serial.println(F("DHT_3 Error!"));
+  }  //if третьего датчика температуры
+
   
-//  float hic2 = dht2.computeHeatIndex(t2, h2, false);
+ #ifdef DEBUG_DHT 
+//Выдача информации о температуре и влажности в серийный порт
+//  Первый датчик
+  Serial.print(F("Humidity1: "));
+  Serial.print(h1);
+  Serial.print(F("% Temperature: "));
+  Serial.print(t1);
+  Serial.println(F("°C"));
+// Второй датчик
   Serial.print(F("Humidity2: "));
   Serial.print(h2);
   Serial.print(F("% Temperature2: "));
   Serial.print(t2);
   Serial.println(F("°C "));
-// Serial.print(hic2);
-//  Serial.print(F("°C "));
-  
- // float hic3 = dht3.computeHeatIndex(t3, h3, false);
+// Третий датчик
   Serial.print(F("Humidity3: "));
   Serial.print(h3);
   Serial.print(F("% Temperature3: "));
   Serial.print(t3);
   Serial.println(F("°C "));
- // Serial.print(hic3);
- // Serial.print(F("°C "));
+#endif
 
-  } //loop
+}  //loop
